9cbe42e0d93590c6751e20b7dffc79a8327dd160
GrafKnuspy <spuckteller@gmail.com>
2025-09-29 11:11:57 +0200

    feat: small dpsy test

---
+++ backend/dspy_test.py
@@ -0,0 +1,25 @@
+# %%
+import dspy
+from dotenv import load_dotenv
+from model_builder import build_lm
+
+load_dotenv()
+
+print("running mini-dspy")
+
+# llama3.2:3b
+lm = build_lm("apertus-70b-com")
+
+dspy.configure(lm=lm)
+
+
+class Base(dspy.Signature):
+    question: str = dspy.InputField()
+    answer: str = dspy.OutputField()
+
+
+model = dspy.Predict(Base)
+
+# %%
+output = model(question="what is the capital of argentina?")
+print(output.answer)
